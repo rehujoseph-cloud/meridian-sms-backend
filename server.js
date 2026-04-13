4649dd8749fba71348e5b2aaacaaaf617f543494
rehujoseph-cloud <rehujoseph@gmail.com>
2026-04-13 19:32:02 +0800

    Update server.js

--- server.js
+++ server.js
@@ -31,5 +31,5 @@ app.get('/', (req, res) => {
   res.json({ status: 'Meridian SMS Backend running ✅' });
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
